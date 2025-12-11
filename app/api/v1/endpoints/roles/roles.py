0664ad092ff7f717ca795806752e7e5bcb02271e
Debdwaipayan Biswas <debdwaipayan02@gmail.com>
2025-12-11 14:40:29 +0530

    Router setup complete

---
+++ app/api/v1/endpoints/roles/roles.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/roles/",
+    tags=["Roles"]
+)
--- app/api/v1/router/router.py
+++ app/api/v1/router/router.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter
-
+from app.api.v1.endpoints.user.user import router as user_router
+from app.api.v1.endpoints.channel.channel import router as channel_router
+from app.api.v1.endpoints.guild.guild import router as guild_router
+from app.api.v1.endpoints.roles.roles import router as roles_router
 router = APIRouter(
     prefix="/v1",
 )
+router.include_router(user_router)
+router.include_router(channel_router)
+router.include_router(guild_router)
+router.include_router(roles_router)
