43e25de574a3794adc01580a19b120e2902333e1
Debdwaipayan Biswas <debdwaipayan02@gmail.com>
2025-12-16 19:25:12 +0530

    Dependency injection in guild endpoints

--- app/api/v1/endpoints/guild/guild.py
+++ app/api/v1/endpoints/guild/guild.py
@@ -4,7 +4,7 @@ from starlette import status
 from loguru import logger
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from app.services.database.database import DatabaseConnect
+from app.services.database.database import DatabaseService,get_db
 from app.api.v1.endpoints.user.helper.user_helper import get_current_user
 from app.api.dependencies.permission.permissions import Permission
 from app.api.v1.endpoints.guild.helper.guild_helper import *
@@ -20,12 +20,13 @@ router = APIRouter(
 )
 limiter= Limiter(key_func=get_remote_address)
 current_user=Annotated[dict, Depends(get_current_user)]
+database=Annotated[DatabaseService,Depends(get_db)]
 
 try:
     @router.post('/create-guild', status_code=status.HTTP_201_CREATED)
     @limiter.limit("5/minute")
-    async def create_guild(request: Request,user:current_user,guild_name:str):
-        if not await DatabaseConnect.user_collection_find_one(user['user_id']):
+    async def create_guild(request: Request,db:database,user:current_user,guild_name:str):
+        if not await db.user_find_one(user['user_id']):
             logger.error(f"User with ID {user['user_id']} not found in database")
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{user['user_name']} not found")
         guild_id=str(uuid.uuid4())
@@ -39,9 +40,9 @@ try:
             "roles_in_guild": [f'{settings.OWNER_ROLE_ID}',f'{settings.MEMBER_ROLE_ID}'],
             "channels": []
         }
-        await DatabaseConnect.guild_collection_insert_one(doc)
+        await db.guild_insert_one(doc)
         logger.info("Guild created successfully")
-        user_doc=await DatabaseConnect.user_collection_find_one(user['user_id'])
+        user_doc=await db.user_find_one(user['user_id'])
         user_guilds=user_doc.get("guilds", [])
         user_guilds.append(guild_id)
         user_roles=user_doc.get("roles", [])
@@ -52,7 +53,7 @@ try:
                 "roles": user_roles
             }
         }
-        await DatabaseConnect.user_collection_update_one(user['user_id'],update_user_doc)
+        await db.user_update_one(user['user_id'],update_user_doc)
         logger.info(f"User {user['user_id']} added as owner to guild {guild_id}")
         return {
             "status": status.HTTP_201_CREATED,
@@ -66,22 +67,22 @@ except:
 try:
     @router.delete('/{guild_id}/delete-guild', status_code=status.HTTP_200_OK)
     @limiter.limit("5/minute")
-    async def delete_guild(guild_id: str, request: Request,user:current_user):
+    async def delete_guild(guild_id: str,db:database, request: Request,user:current_user):
         current_user_id=user['user_id']
         current_user_name=user['user_name']
         await ValidUserCheck(current_user_id,current_user_name,guild_id,"guild_owner")
-        guild_doc=await DatabaseConnect.guild_collection_find_one(guild_id)
+        guild_doc=await db.guild_find_one(guild_id)
         guild_channels=guild_doc.get("channels", [])
         for channel_id in guild_channels:
-            await DatabaseConnect.channel_collection_delete_one(channel_id)
+            await db.channel_delete_one(channel_id)
             logger.info(f"Deleted channel id:{channel_id} from guild id:{guild_id}")
         roles_list=guild_doc.get("roles_in_guild",[])
         for role_id in roles_list:
-            await DatabaseConnect.role_collection_delete_one(role_id)
+            await db.role_delete_one(role_id)
             logger.info(f"Deleted role id:{role_id} from guild id:{guild_id}")
         user_list=guild_doc.get("users", [])
         for user_id in user_list:
-            user_doc=await DatabaseConnect.user_collection_find_one(user_id)
+            user_doc=await db.user_find_one(user_id)
             user_guilds=user_doc.get("guilds", [])
             user_roles=user_doc.get("roles",[])
             for user_role in user_roles:
@@ -94,9 +95,9 @@ try:
                             "roles":user_roles
                         }
                     }
-                    await DatabaseConnect.user_collection_update_one(user_id,update_user_doc)
+                    await db.user_update_one(user_id,update_user_doc)
                     break
-        await DatabaseConnect.guild_collection_delete_one(guild_id)
+        await db.guild_delete_one(guild_id)
         logger.info(f"Guild {guild_id} deleted successfully")
         return {
             "status": status.HTTP_200_OK,
@@ -109,7 +110,7 @@ except:
 try:
     @router.put('/{guild_id}/trasfer-owner',status_code=status.HTTP_202_ACCEPTED)
     @limiter.limit("3/minute")
-    async def transfer_guild_ownership(guild_id: str, new_owner_id: str, request: Request,user:current_user):
+    async def transfer_guild_ownership(guild_id: str, new_owner_id: str, request: Request,db:database,user:current_user):
         current_user_id=user['user_id']
         current_user_name=user['user_name']
         current_user_doc=await ValidUserCheck(current_user_id,current_user_name,guild_id,"guild_owner")
@@ -129,7 +130,7 @@ try:
                         "roles": new_owner_roles
                     }
                 }
-                await DatabaseConnect.user_collection_update_one(new_owner_id,update_user_doc)
+                await db.user_update_one(new_owner_id,update_user_doc)
                 break
         current_user_roles=current_user_doc.get("roles", [])
         for current_user_role in current_user_roles:
@@ -141,14 +142,14 @@ try:
                         "roles": current_user_roles
                     }
                 }
-                await DatabaseConnect.user_collection_update_one(current_user_id,update_user_doc)
+                await db.user_update_one(current_user_id,update_user_doc)
                 break
         update_guild_doc={
             "$set": {
                 "owner_id": new_owner_id
             }
         }
-        await DatabaseConnect.guild_collection_update_one(guild_id,update_guild_doc)
+        await db.guild_update_one(guild_id,update_guild_doc)
         logger.info(f"Guild_ID:{guild_id} ownership transferred to user {new_owner_id} successfully")
         return {
             "status": status.HTTP_202_ACCEPTED,
@@ -161,14 +162,14 @@ except:
 try:
     @router.get('/{guild_id}/guild-details', status_code=status.HTTP_200_OK)
     @limiter.limit("10/minute")
-    async def get_guild_info(guild_id: str, request: Request,user:current_user):
+    async def get_guild_info(guild_id: str, request: Request,db:database,user:current_user):
         user_id=user['user_id']
         user_name=user['user_name']
         user_doc=await isUserinGuild(user_id, guild_id)
         if not user_doc:
             logger.error(f"User{user_name} with ID {user_id} is not a member of guild {guild_id}")
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"User{user_id} is not a member of the guild")
-        guild_doc=await DatabaseConnect.guild_collection_find_one(guild_id)
+        guild_doc=await db.guild_find_one(guild_id)
         logger.info(f"Fetched information for Guild_ID: {guild_id} successfully")
         return {
             "status": status.HTTP_200_OK,
@@ -182,7 +183,7 @@ except:
 try:
     @router.put('/{guild_id}/update-guild-name', status_code=status.HTTP_200_OK)
     @limiter.limit("5/minute")
-    async def update_guild_name(guild_id: str, new_guild_name: str, request: Request,user:current_user):
+    async def update_guild_name(guild_id: str, new_guild_name: str, request: Request,db:database,user:current_user):
         user_id=user['user_id']
         user_name=user['user_name']
         await ValidUserCheck(user_id,user_name,guild_id,"mod_guild")
@@ -191,7 +192,7 @@ try:
                 "guild_name": new_guild_name
             }   
         }
-        await DatabaseConnect.guild_collection_update_one(guild_id,update_guild_doc)
+        await db.guild_update_one(guild_id,update_guild_doc)
         logger.info(f"Guild_ID: {guild_id} name updated to {new_guild_name} successfully")
         return {
             "status": status.HTTP_200_OK,
@@ -204,14 +205,14 @@ except:
 try:
     @router.put("/{guild_id}/add-user", status_code=status.HTTP_200_OK)
     @limiter.limit("10/minute")
-    async def add_user_to_guild(guild_id: str, new_member_user_id: str, request: Request,user:current_user):
+    async def add_user_to_guild(guild_id: str, new_member_user_id: str, request: Request,db:database,user:current_user):
         current_user_id=user['user_id']
         current_user_name=user['user_name']
         await ValidUserCheck(current_user_id,current_user_name,guild_id,"add_guild")
-        new_member_doc=await DatabaseConnect.user_collection_find_one(new_member_user_id)
+        new_member_doc=await db.user_find_one(new_member_user_id)
         if not new_member_doc:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User id not registered as user")
-        guild_doc=await DatabaseConnect.guild_collection_find_one(guild_id)
+        guild_doc=await db.guild_find_one(guild_id)
         users_list=guild_doc.get("users", [])
         for user_id in users_list:
             if user_id==new_member_user_id:
@@ -233,8 +234,8 @@ try:
                 "roles": new_member_roles
             }
         }
-        await DatabaseConnect.user_collection_update_one(new_member_user_id,update_user_doc)
-        await DatabaseConnect.guild_collection_update_one(guild_id,update_guild_doc)
+        await db.user_update_one(new_member_user_id,update_user_doc)
+        await db.guild_update_one(guild_id,update_guild_doc)
         logger.info(f"User_ID: {new_member_user_id} added to Guild_ID: {guild_id} successfully")
         return {
             "status": status.HTTP_200_OK,
@@ -247,14 +248,14 @@ except:
 try:
     @router.put("/{guild_id}/remove-user", status_code=status.HTTP_200_OK)
     @limiter.limit("10/minute")
-    async def remove_user_from_guild(guild_id: str, member_user_id: str, request: Request,user:current_user):
+    async def remove_user_from_guild(guild_id: str, member_user_id: str, request: Request,db:database,user:current_user):
         current_user_id=user['user_id']
         current_user_name=user['user_name']
         await ValidUserCheck(current_user_id,current_user_name,guild_id,"kick_member")
-        member_user_doc=await DatabaseConnect.user_collection_find_one(member_user_id)
+        member_user_doc=await db.user_find_one(member_user_id)
         if not member_user_doc:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User id not registered as user")
-        guild_doc=await DatabaseConnect.guild_collection_find_one(guild_id)
+        guild_doc=await db.guild_find_one(guild_id)
         user_list=guild_doc.get("users", [])
         if member_user_id not in user_list:
             logger.error(f"User with ID {member_user_id} is not a member of guild {guild_id}")
@@ -278,8 +279,8 @@ try:
                 "roles": member_user_roles
             }
         }
-        await DatabaseConnect.user_collection_update_one(member_user_id,update_user_doc)
-        await DatabaseConnect.guild_collection_update_one(guild_id,update_guild_doc)
+        await db.user_update_one(member_user_id,update_user_doc)
+        await db.guild_update_one(guild_id,update_guild_doc)
         logger.info(f"User_ID: {member_user_id} removed from Guild_ID: {guild_id} successfully")
         return {
             "status": status.HTTP_200_OK,
